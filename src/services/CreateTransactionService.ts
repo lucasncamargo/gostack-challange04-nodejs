cf22de31eec64a0adb07bf45034677dd25d696ad
Lucas Camargo <lucasn.camargo@gmail.com>
2020-05-09 00:06:42 -0300

    List e Create de uma Transaction com todas as regras de negócio validadas

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type === 'outcome') {
+      const { total } = this.transactionsRepository.getBalance();
+      if (value > total) {
+        throw new Error(
+          'Your transaction amount exceeds the available balance.',
+        );
+      }
+    }
+    const transaction = new Transaction({ title, value, type });
+    return this.transactionsRepository.create(transaction);
   }
 }
 
